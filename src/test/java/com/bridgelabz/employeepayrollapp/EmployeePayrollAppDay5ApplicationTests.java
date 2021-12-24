a047bd3d3557acd9a1651380841d9810f6dc7b78
amolnagose12 <amolnagose12@gmail.com>
2021-12-24 16:08:51 +0530

    Introducing DTO and model to Employee payroll app

---
+++ src/test/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppDay5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayrollapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollAppDay5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
